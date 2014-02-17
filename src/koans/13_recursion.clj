23d1c4a3509c94b88dde34691a0ce089c12ab6a4
Meagan Waller <meaganewaller@gmail.com>
2014-02-17 15:52:41 -0600

    finish clojure koans

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,28 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         reversed ()]
+    (if (= () coll)
+      reversed
+      (recur (rest coll) (cons (first coll) reversed)))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc 1]
+    (if (= 0 n)
+      acc
+      (recur (dec n) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
--- src/koans/21_group_by.clj
+++ src/koans/21_group_by.clj
@@ -1,11 +1,11 @@
 (defn get-odds-and-evens [coll]
-  (let [{odds true evens false} (group-by __ coll)]
+  (let [{odds true evens false} (group-by odd? coll)]
     [odds evens]))
 
 (meditations
 
   "To categorize a collection by some function, use group-by."
-  (= __ (group-by count ["hello" "world" "foo" "bar"]))
+  (= {5["hello" "world"] 3["foo" "bar"]} (group-by count ["hello" "world" "foo" "bar"]))
 
 
   "You can simulate filter + remove in one pass"
@@ -14,7 +14,9 @@
      [[1 3 5] [2 4]])
 
   "You can also group by a primary key"
-  (= __
+  (= {1[{:name "Bob" :id 1}
+        {:last-name "Smith" :id 1}]
+      2[{:name "Mike" :id 2}]}
      (group-by :id [{:id 1 :name "Bob"}
                     {:id 2 :name "Mike"}
                     {:id 1 :last-name "Smith"} ]))
@@ -22,13 +24,15 @@
   "But be careful when you group by non-required key"
   (= {"Bob" [{:name "Bob" :id 1}]
       "Mike" [{:name "Mike" :id 2}]
-      __ [{:last-name "Smith" :id 1}]}
+      nil [{:last-name "Smith" :id 1}]}
    (group-by :name [{:id 1 :name "Bob"}
                     {:id 2 :name "Mike"}
                     {:id 1 :last-name "Smith"}]))
 
   "The true power of group-by comes with custom functions"
-  (= __
+  (= {:naughty-list [{:name "Jimmy" :bad true}
+                     {:name "Joe" :bad true }]
+      :nice-list [{ :name "Jack" :bad false}]}
      (group-by #(if (:bad %) :naughty-list :nice-list)
                [{:name "Jimmy" :bad true}
                 {:name "Jack" :bad false}
