2754ce014e9de668030d8351e2af8101b1e06424
Meagan Waller <meaganewaller@gmail.com>
2014-02-17 09:34:06 -0600

    First koan done

--- src/koans/01_equalities.clj
+++ src/koans/01_equalities.clj
@@ -1,6 +1,6 @@
 (meditations
   "We shall contemplate truth by testing reality, via equality"
-  (= __ true)
+  (= true true)
 
   "To understand reality, we must compare our expectations against reality"
   (= __ (+ 1 1))
